d5e410dcd50036ebab574e79c46f7c9143e8ff76
yagi0153 <127298626+yagi0153@users.noreply.github.com>
2025-03-05 17:57:08 +0900

    UI進捗
    更新しました。

---
+++ Assets/Aoyagi/Scripts/Timer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Timer : MonoBehaviour
+{
+
+    //ここからゲーム終了の処理を実行
+    public void GameEnd()
+    {
+        Debug.Log("ゲームオーバー");
+    }
+}
