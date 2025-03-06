3b386d5f8b1563a2b100eb539c159e58a108604d
etsuryoga <passyuki0507@gmail.com>
2025-03-06 18:08:14 +0900

    立ち絵変化

--- Assets/Etsu/Player.cs
+++ Assets/Etsu/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
 
     public int count = 0;
     bool swing = false;
-    public float space_cool_time = 1;
+    public float space_cool_time = 1; //クールタイム
 
     public GameObject santa1;
     public GameObject santa2;
@@ -26,18 +26,30 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) || (Input.GetMouseButtonDown(0)) && swing == false)
         {
-            santa1.SetActive(true);
-            santa2.SetActive(false);
+            santa1.SetActive(false);
+            santa2.SetActive(true);
             santa3.SetActive(false);
+            swing = true;
+            StartCoroutine(SpaceCooldown());
+
         }
+
     }
 
 
     IEnumerator SpaceCooldown() // スペースキーのクールタイム
     {
         Debug.Log("クールタイム中...");
+        yield return new WaitForSeconds(0.2f);
+
+        santa1.SetActive(false);
+        santa2.SetActive(false);
+        santa3.SetActive(true);
+
         yield return new WaitForSeconds(space_cool_time); // f秒待つ
-        StartCoroutine(SpaceCooldown()); // クールタイム開始
+        santa1.SetActive(true);
+        santa2.SetActive(false);
+        santa3.SetActive(false);
         swing = false; // スペースキーを再び押せるようにする
         Debug.Log("クールタイム終了！スペースキーが再び使えます");
     }
