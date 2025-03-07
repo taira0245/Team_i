a15c3a6409459175c7f9a9ae15f2295c71772dd7
etsuryoga <passyuki0507@gmail.com>
2025-03-07 15:25:41 +0900

    Update Player.cs

--- Assets/Etsu/Player.cs
+++ Assets/Etsu/Player.cs
@@ -3,14 +3,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int hp = 3;
-    public int count = 0;
+    [Header("ステータス")]
+    [Tooltip("体力")]
+    [SerializeField] public int hp = 3;
+    [Tooltip("撃墜数")]
+    [SerializeField] public int count = 0;
+    
     bool swing = false;
+    [Tooltip("クールタイム時間")]
     public float space_cool_time = 1; // クールタイム
 
+    [Header("バッティング")]
     public GameObject santa1;
     public GameObject santa2;
     public GameObject santa3;
+
+    [Header("背景")]
+    [Tooltip("最後まで残ります")]
+    [SerializeField] public GameObject purezent1;
+    [Tooltip("体力2まで表示します")]
+    [SerializeField] public GameObject purezent2;
+    [Tooltip("体力最大時(3)のみ表示します")]
+    [SerializeField] public GameObject purezent3;
+
 
     void Update()
     {
@@ -18,6 +33,28 @@ public class Player : MonoBehaviour
         {
             StartCoroutine(Swing());
         }
+        
+        if(hp == 3)
+        {
+            purezent1.SetActive(true);
+            purezent2.SetActive(true);
+            purezent3.SetActive(true);
+        }
+        if (hp == 2)
+        {
+            purezent3.SetActive(false);
+        }
+        if (hp == 1)
+        {
+            purezent2.SetActive(false);
+
+        }
+        if (hp == 0)
+        {
+            purezent1.SetActive(false);
+        }
+
+
     }
 
     IEnumerator Swing()
@@ -45,4 +82,6 @@ public class Player : MonoBehaviour
 
         Debug.Log("クールタイム終了！スペースキーが再び使えます");
     }
+
+
 }
