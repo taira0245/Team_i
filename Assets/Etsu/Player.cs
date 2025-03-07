f30b1fcdb5b615b6140c5e56ed59b84bef446588
etsuryoga <passyuki0507@gmail.com>
2025-03-07 14:48:15 +0900

    サンタの修正

--- Assets/Etsu/Player.cs
+++ Assets/Etsu/Player.cs
@@ -1,57 +1,48 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public int hp = 3;
-
     public int count = 0;
     bool swing = false;
-    public float space_cool_time = 1; //クールタイム
+    public float space_cool_time = 1; // クールタイム
 
     public GameObject santa1;
     public GameObject santa2;
     public GameObject santa3;
 
-
     void Update()
     {
-        Swing();
-    }
-
-
-
-    void Swing()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) || (Input.GetMouseButtonDown(0)) && swing == false)
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && !swing)
         {
-            santa1.SetActive(false);
-            santa2.SetActive(true);
-            santa3.SetActive(false);
-            swing = true;
-            StartCoroutine(SpaceCooldown());
-
+            StartCoroutine(Swing());
         }
-
     }
 
-
-    IEnumerator SpaceCooldown() // スペースキーのクールタイム
+    IEnumerator Swing()
     {
+        swing = true; // スイング開始
+        Debug.Log("攻撃開始");
+
+        santa1.SetActive(false);
+        santa2.SetActive(true);
+        santa3.SetActive(false);
+
         Debug.Log("クールタイム中...");
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.1f);
 
         santa1.SetActive(false);
         santa2.SetActive(false);
         santa3.SetActive(true);
 
-        yield return new WaitForSeconds(space_cool_time); // f秒待つ
+        yield return new WaitForSeconds(space_cool_time);
+
         santa1.SetActive(true);
         santa2.SetActive(false);
         santa3.SetActive(false);
-        swing = false; // スペースキーを再び押せるようにする
+        swing = false; // クールタイム終了後、再度攻撃可能に
+
         Debug.Log("クールタイム終了！スペースキーが再び使えます");
     }
-
 }
