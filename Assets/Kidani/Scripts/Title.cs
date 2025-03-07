592677acfa372cd9693c45122341b09351365bea
kidaniao <kidaniao982@gmail.com>
2025-03-07 16:31:50 +0900

    シーン遷移にフェイドをいれた。など

--- Assets/Kidani/Scripts/Title.cs
+++ Assets/Kidani/Scripts/Title.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class Title : MonoBehaviour
 {
     [SerializeField] string next_scene_name = default!;
-
+    [SerializeField] ScreenFade screenfade;
+    [SerializeField,Range(0,2)] float fadetime;
     public void TitleButton()
     {
-        SceneManager.LoadScene(next_scene_name);
+        //SceneManager.LoadScene(next_scene_name);
+        screenfade.SetSceneChange(next_scene_name,fadetime);
     }
 
 }
