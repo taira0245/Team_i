9ba625211a90d7a1d4801dfa69f2b8399c9edc66
yagi0153 <127298626+yagi0153@users.noreply.github.com>
2025-03-07 16:23:21 +0900

    進捗(ゲームスタート時のアニメーション)
    ゲームスタート時のアニメーションUIの表示・非表示とプレゼントボックスとHPの連動をしました。
    デザイナーさんのUIが届き次第、アニメーションを制作して更新します。

--- Assets/Aoyagi/Scripts/GameEndAnimationUI.cs
+++ Assets/Aoyagi/Scripts/GameEndAnimationUI.cs
@@ -2,28 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartAndEndAnimation : MonoBehaviour
+public class GameEndAnimationUI : AnimationUI
 {
-    [Header("スタートUIのアニメーション"), SerializeField]
-    private Animator animator;
-
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
-    //この関数が実行するとアニメーションを再生
-    public void PlayUIAnimation()
+    //デバッグ用
+    public void Update()
     {
-        animator.SetTrigger("anim_start");
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PlayUIAnimation();
+        }
+    }
+
+    //この関数が実行するとアニメーションを再生
+    public override void PlayUIAnimation()
+    {
+        base.PlayUIAnimation();
     }
 
     //ゲームスタートのアニメーションが終了した時、この関数が実行される。
     //ここに次の処理を入れるとアニメーションが終わった時に実行されます。
-    //例：(ゲーム全体の一時停止・再生)
+    //例：(ゲーム全体の一時停止)
     public void UIAnimationEnd()
     {
         Debug.Log("アニメーション終了");
-        this.gameObject.SetActive(false);
     }
 }
